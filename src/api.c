027a5595190488525cdf5ff945a2af20600a080b
Alej4ndro <you@fakemail.com>
2015-12-11 13:58:28 +0000

    Latest changes in the api

--- src/api.c
+++ src/api.c
@@ -1,35 +1,43 @@
 #include "api.h"
 
-void api_rotate(int angle) {
+void api_rotate(Terrain *terrain, int angle) {
     switch(angle) {
-        case 90: rotate_90(); break;
-        case 180: rotate_180(); break;
-        case 270: rotate_270(); break;
+        case 90: rotate_90(terrain); break;
+        case 180: rotate_180(terrain); break;
+        case 270: rotate_270(terrain); break;
     }
 }
 
-void rotate_90() {
-  int x,y,map_offset,old_height,old_width;
-  int map_size=WIDTH*HEIGHT;
-  if (!terrain_height)return;
+/*
+  0 1 2      0 1    1 2 3 5 6 7
+0 1 2 3    0 5 1    5 1 6 2 7 3
+1 5 6 7 -> 1 6 2
+           2 7 3
+*/
+void rotate_90(Terrain *terrain) {
+    Point *point = NULL;
+    int x,y,map_offset;
 
-  old_height=HEIGHT;
-  old_width=WIDTH;
-  HEIGHT=WIDTH;
-  WIDTH=old_height;
-  map_offset=0;
-  for (y = 0; y<old_height; y++) {
-		map_offset=old_height-1-y;
+    int oldVerSize = terrain->verSize;
+    int oldHorSize = terrain->horSize;
+    int verSize = oldHorSize;
+    int horSize = oldVerSize;
 
-		for (x = 0; x<old_width; x++) {
-		  map_offset+=WIDTH;
-		}
-	}
+    Terrain *newTerrain = generateTerrain(horSize, verSize);
 
-	undo=total_undo_swap_x_y;
+    for(x = 0; x < horSize; x++) {
+        for(y = 0; y < verSize; y++) {
+            oldPoint = getPoint(oldTerrain, y, x);
+            newPoint = getPoint(newTerrain, x, y);
+            newPoint->height = oldPoint->height;
+        }
+    }
+
+    free(terrain);
+    terrain = newTerrain;
 }
 
-void rotate_180() {
+void rotate_180(Terrain *terrain) {
   int x,y,map_offset;
   int map_size=WIDTH*HEIGHT;
   if (!terrain_height)return;
@@ -43,7 +51,7 @@ void rotate_180() {
 	}
 }
 
-void rotate_270() {
+void rotate_270(Terrain *terrain) {
   int x,y,map_offset,old_height,old_width;
   int map_size=WIDTH*HEIGHT;
   if (!terrain_height)return;
