c013605f1175898f17b4b14f73ec2534c2656586
Alej4ndro <you@fakemail.com>
2016-02-17 22:24:30 +0000

    Extracted functions for the loading of hme

---
+++ src/hme_load.h
@@ -0,0 +1,12 @@
+#ifndef HME_LOAD_H
+#define HME_LOAD_H
+
+#include "main.h"
+
+HeightMapEditor *loadHeightMapEditor();
+SDL_Window *createWindow(char *title, int width, int height);
+SDL_Renderer *createRenderer(SDL_Window *window);
+void freeResources(HeightMapEditor *hme);
+TerrainVisualParams *loadTerrainParams(SDL_Rect *paramsRect);
+
+#endif
