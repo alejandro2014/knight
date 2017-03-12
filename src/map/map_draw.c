3af0a880b83723ee863ffb78becc85f230f3f3a0
Alejandro <alejandro@desktop>
2017-03-12 23:06:52 +0100

    Perfect grid

--- src/map/map_draw.c
+++ src/map/map_draw.c
@@ -14,16 +14,25 @@ void drawTerrain(Screen *screen, Terrain *terrain, TerrainVisualParams *params) 
     int width = 600;
     int height = 300;
 
-    int i;
+    int currentX = 0;
+    int currentY = 0;
 
-    for (i = 0; i * 10 < width; i++) {
-        SDL_RenderDrawLine(renderer, xOffset + (i * 10), yOffset, xOffset + (i * 10), height);
+    int i, j;
+
+    for (i = 0; currentX < width; i++) {
+        currentX = i * 10;
+        SDL_RenderDrawLine(renderer, xOffset + currentX, yOffset, xOffset + currentX, yOffset + height);
     }
 
-    SDL_RenderDrawLine(renderer, 10, 40, width, 40);
+    for (j = 0; currentY < height; j++) {
+        currentY = j * 10;
+        SDL_RenderDrawLine(renderer, xOffset, yOffset + currentY, xOffset + width, yOffset + currentY);
+    }
+
+    /*SDL_RenderDrawLine(renderer, 10, 40, width, 40);
     SDL_RenderDrawLine(renderer, 10, 50, width, 50);
     SDL_RenderDrawLine(renderer, 10, 60, width, 60);
-    SDL_RenderDrawLine(renderer, 10, 70, width, 70);
+    SDL_RenderDrawLine(renderer, 10, 70, width, 70);*/
     /*if(!terrain) return;
 
     calculateOffset(terrain, &xOffset, &yOffset, params);
