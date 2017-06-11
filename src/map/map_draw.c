c927e27ff3e0953f8208d1683f052fcf1c155191
Alejandro <alejandro@desktop>
2017-06-11 13:27:32 +0200

    Drawn barcode without guard bars

--- src/map/map_draw.c
+++ src/map/map_draw.c
@@ -21,8 +21,7 @@ void drawRuler(SDL_Renderer *renderer, int widthFactor, int offset) {
     }
 }
 
-void drawBarcodeDigit(SDL_Renderer *renderer, char digit, int offset) {
-    int widthFactor = 10;
+void drawBarcodeDigit(SDL_Renderer *renderer, char digit, int offset, int widthFactor) {
     int i, j;
     char temp = 0x00;
 
@@ -55,12 +54,19 @@ int getParity(char ctrlDigit, int position) {
     return parities[controlDigit][position];
 }
 
-char getBarcodeDigit(char number, int isOdd) {
+char getBarcodeDigitLeftSide(char number, int isOdd) {
     char odd[] = { 0x0d, 0x19, 0x13, 0x3d, 0x23, 0x31, 0x2f, 0x3b, 0x37, 0x0b };
     char even[] = { 0x27, 0x33, 0x1b, 0x21, 0x1d, 0x39, 0x05, 0x11, 0x09, 0x17 };
     int digit = number - 0x30;
 
     return (isOdd ? odd[digit] : even[digit]);
+}
+
+char getBarcodeDigitRigthSide(char number) {
+    char digitsArray[] = { 0x72, 0x66, 0x6c, 0x42, 0x5c, 0x4e, 0x50, 0x44, 0x48, 0x74 };
+    int digit = number - 0x30;
+
+    return digitsArray[digit];
 }
 
 void drawTerrain(Screen *screen, Terrain *terrain, TerrainVisualParams *params) {
@@ -70,14 +76,21 @@ void drawTerrain(Screen *screen, Terrain *terrain, TerrainVisualParams *params) 
     char digit;
     int parity;
     char *leftDigits = "501031";
+    char *rightDigits = "311309";
 
     int i;
+    int widthFactor = 2;
     clearSubScreen(renderer, &(screen->terrainCoords), &(screen->bgColorTerrain));
 
     for(i = 0; i < 6; i++) {
-        parity = getParity('7', 0);
-        digit = getBarcodeDigit(*(leftDigits + i), parity);
-        drawBarcodeDigit(renderer, digit, 100 * i + 10);
+        parity = getParity('7', i);
+        digit = getBarcodeDigitLeftSide(*(leftDigits + i), parity);
+        drawBarcodeDigit(renderer, digit, 7 * widthFactor * i + 10, widthFactor);
+    }
+
+    for(i = 0; i < 6; i++) {
+        digit = getBarcodeDigitRigthSide(*(rightDigits + i));
+        drawBarcodeDigit(renderer, digit, 7 * widthFactor * i + 100 + 10, widthFactor);
     }
 
     //drawBarcodeDigit(renderer, '3', 220);
--- src/map/map_draw.h
+++ src/map/map_draw.h
@@ -15,9 +15,10 @@ void calculateOffset(Terrain *terrain, int *xOffset, int *yOffset, TerrainVisual
 
 void drawRuler(SDL_Renderer *renderer, int widthFactor, int offset);
 void drawBarcodeLine(SDL_Renderer *renderer, int x);
-void drawBarcodeDigit(SDL_Renderer *renderer, char digit, int offset);
+void drawBarcodeDigit(SDL_Renderer *renderer, char digit, int offset, int widthFactor);
 
-char getBarcodeDigit(char number, int isOdd);
+char getBarcodeDigitLeftSide(char number, int isOdd);
+char getBarcodeDigitRightSide(char number);
 int getParity(char ctrlDigit, int position);
 
 #endif
